b6b83320dec27a36b1b73bfc0d70a4ef4ae480f2
xinrong7719 <HXR_1993@hotmail.com>
2023-08-28 15:09:00 +0800

    v1.1

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <React.StrictMode>
+
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
